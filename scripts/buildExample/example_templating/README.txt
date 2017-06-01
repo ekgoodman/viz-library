b64a4ae759fbf7151db8209fa44cac8b3934ccb6
James Oden <joden1995@gmail.com>
2017-06-01 16:14:41 -0400

    visual_test_suites_added
    summary visual tests coming soon

--- scripts/buildExample/example_templating/README.txt
+++ scripts/buildExample/example_templating/README.txt
@@ -9,6 +9,5 @@ Instructions:
 1.  Install Python
 2.  Install jinja2 (Ensure Python recognizes the jinja module)
    ^ Don't spend more than 15 min on the first two instructions like I did lol
-3.  Open the python IDLE and run chart_gen.py
-4.  Execute the file_maniputlaion sript (double click on it in explorer)
-5.  0014_example_etc will now have the updated testConfig.js
+3.  Execute the generate_visual_examples sript (double click on it in explorer)
+4.  0014_example_etc will now have the updated testConfig.js
---
+++ scripts/buildExample/example_templating/generate_visual_examples.bat
@@ -0,0 +1,13 @@
+python chart_gen.py
+
+DEL "..\..\..\examples\0014-webcharts-visual-tests\01_scatter_plot\testConfig.js"
+move scatter_plot_testConfig.js ..\..\..\examples\0014-webcharts-visual-tests\01_scatter_plot\testConfig.js
+
+DEL "..\..\..\examples\0014-webcharts-visual-tests\02_bar_chart\testConfig.js"
+move bar_chart_testConfig.js ..\..\..\examples\0014-webcharts-visual-tests\02_bar_chart\testConfig.js
+
+DEL "..\..\..\examples\0014-webcharts-visual-tests\03_config_settings\testConfig.js"
+move config_settings_testConfig.js ..\..\..\examples\0014-webcharts-visual-tests\03_config_settings\testConfig.js
+
+DEL "..\..\..\examples\0014-webcharts-visual-tests\04_time_axis\testConfig.js"
+move time_axis_testConfig.js ..\..\..\examples\0014-webcharts-visual-tests\04_time_axis\testConfig.js
